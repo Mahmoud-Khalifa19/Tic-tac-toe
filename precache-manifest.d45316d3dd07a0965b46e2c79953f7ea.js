c5f2099df79fadf603d561ca19d497b4feb69e5a
mahmoud khalifa <mahmoudbkhalifa@gmail.com>
2020-10-23 23:07:30 +0100

    Updates

---
+++ precache-manifest.d45316d3dd07a0965b46e2c79953f7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c9525183de706f8b61bb4de1c21dec",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "04a7b0fbc2ef72949275",
+    "url": "/Tic-tac-toe/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "298b2204f44f2ddd375d",
+    "url": "/Tic-tac-toe/static/js/2.ccaf51cc.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Tic-tac-toe/static/js/2.ccaf51cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04a7b0fbc2ef72949275",
+    "url": "/Tic-tac-toe/static/js/main.f6feec2f.chunk.js"
+  },
+  {
+    "revision": "761fab3e8c0c4cd8f9a5",
+    "url": "/Tic-tac-toe/static/js/runtime-main.df623b12.js"
+  }
+]);
